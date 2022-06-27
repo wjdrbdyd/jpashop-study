17f969602bcd7a9dd23cfbc9e2232931607e9d49
wjdrbdyd <jky9010@gmail.com>
2022-06-27 17:23:50 +0900

    도메인 개발완료

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+    private int price;
+    private int stockQuantity;
+    private String name;
+    private String author;
+    private String isbn;
+
+}
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -20,6 +20,15 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    @Transactional
+    public Item updateItem(Long itemId,String name, int price, int stockQuantity ) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        return findItem;
+    }
+
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
